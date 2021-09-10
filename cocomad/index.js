815553ca1406bfecedb91fecb915e7a3e32338fe
NicoMen <nicomendoza.92@gmail.com>
2021-09-10 19:44:29 +0200

    se agrego estilos

--- cocomad/index.js
+++
@@ -1,8 +0,0 @@
-function navbarMenu() {
-    let x = document.getElementById("myNavbar");
-    if (x.className === "barranav") {
-      x.className += " responsive";
-    } else {
-      x.className = "barranav";
-    }
-  }
